b41d4fa9c871ae2427565392f43cbf51330b8a91
zerai <teclaizerai@gmail.com>
2022-01-03 19:59:39 +0100

    Added basic login with in_memory provider.

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController
+{
+    #[Route('/logout', name: 'placeholder_logout', methods: ['GET'])]
+    public function logout()
+    {
+    }
+}
