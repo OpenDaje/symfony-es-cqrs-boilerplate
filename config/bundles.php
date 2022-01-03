8e383b560a3e2595a5b6b77759119e93eebfd078
zerai <teclaizerai@gmail.com>
2022-01-03 22:32:35 +0100

    Added ecotone/symfony-bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
 ];
