6a64df9ed41b761ba7ffe93a346b6fa008330387
zerai <teclaizerai@gmail.com>
2022-01-31 07:34:32 +0100

    Composer: added lexik/jwt-authentication-bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
